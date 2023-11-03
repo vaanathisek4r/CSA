a8337b3937f86342af4299204396eae314162bf6
vaanathisek4r <vaanathisekar@billericak12.com>
2023-11-03 13:59:10 -0400

    11/3 didn't finish in class

--- Conditionals.java
+++ Conditionals.java
@@ -1,13 +1,27 @@
 public class Conditionals {
     public static void main(String [] arg){
         Conditionals ab = new Conditionals();
-        System.out.println(chimpTrouble(true, true));
-        System.out.println(chimpTrouble(false, false));
-        System.out.println(chimpTrouble(true, false));
-        System.out.println(chimpTrouble(false, true));
+       /*  System.out.println(ab.chimpTrouble(true, true));
+        System.out.println(ab.chimpTrouble(false, false));
+        System.out.println(ab.chimpTrouble(true, false));
+        System.out.println(ab.chimpTrouble(false, true));*/
 
-        System.out.println(negPos(1,-1, false));
+        /*System.out.println(ab.negPos(1,-1, false));
+        System.out.println(ab.negPos(-1,1, false));
+        System.out.println(ab.negPos(1,1, false));
+        System.out.println(ab.negPos(-1,-1, false));
+        System.out.println(ab.negPos(-4,-5, false));
+        System.out.println(ab.negPos(-4,5, false));
+        System.out.println(ab.negPos(4,5, false));
+        System.out.println(ab.negPos(4,-5, false));
+        */
 
+        System.out.println(ab.pickUpPhone(false, false, true));
+        System.out.println(ab.pickUpPhone(true, true, true));
+        System.out.println(ab.pickUpPhone(true, true, false));
+        System.out.println(ab.pickUpPhone(false, true, false));
+        System.out.println(ab.pickUpPhone(true, false, false));
+        System.out.println(ab.pickUpPhone(false, false, false));
     }
     
     public boolean chimpTrouble(boolean aSmile,boolean bSmile){
@@ -24,11 +38,30 @@ public class Conditionals {
         if (negative == true){
             return (num1<0 && num2<0);
         }
+
+        
         else if((num1>0 && num2<0) || (num1<0 && num2>0)){
             return true;
         }
+        
+        else{
+            return false;
+        } 
+    } 
+
+    public boolean pickUpPhone(boolean isMorning, boolean isMom, boolean isAsleep){
+        if (isMorning == true && isMom == true){
+            return true;
+        
+        else if (isAsleep == true){
+            return false;
+
         else{
             return false;
         }
-    } 
+        }
+        }
+    }
+
+
 }

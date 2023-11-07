c3bc6bc1c9e25d53cd87154163ff8a7b286d021c
vaanathisek4r <vaanathisekar@billericak12.com>
2023-11-07 14:32:06 -0500

    done

--- Circle.java
+++ Circle.java
@@ -1,3 +1,17 @@
 public class Circle {
+    private double radius;
+    
+    public Circle(double radius){
+        this.radius = radius;
+    }
+    
+    public double Area(){
+        
+        return Math.PI * Math.pow(radius, 2);
+    }
+    public double circumf(){
+        return 2 * radius * Math.PI;
+        
+    }
 
 }

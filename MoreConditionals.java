8f2a3ff5771feee4cab9a64a9b495a1df94e75e4
vaanathisek4r <vaanathisekar@billericak12.com>
2023-11-15 22:50:49 -0500

    done 11/15

--- MoreConditionals.java
+++ MoreConditionals.java
@@ -12,29 +12,28 @@ public class MoreConditionals {
     }
 
     public int findBestFit(int space, int size1, int size2){
-        if (size1<size2 && space%size1 == 0) return space/size1;
-        else if (size2<size1){
-            if (space%size2 == 0) return space/size2;
-            
+        if (size1+size2<=space) return 3;
+        else if (size1==size2) return 1;
+        else if (size1<size2){
+            if (size1>space || size2>space) return 0;
+            else return 2;
         }
-        else if (size)
-        
-
+        else if (size2<=size1){
+            if (size2>space || size1>space) return 0;
+            else return 1;
+        }
+        else return 0;
     }
 
-    // space is the amount of available space on a memory card.  size1 and size2 are the sizes of two files.
-    // Write a method that takes these integer numbers as parameters and figures out the largest combinations of files that fits on the card.
-    // The method should return 3 if both files fit together, the file number (1 or 2) corresponding to the largest file that fits by itself (1 if they are the same size)
-    // or 0 if neither file fits on the card. 
-
-
-
-
+    public boolean makeBenches(int small, int big, int goal){
+        if (small+big*5 >= goal) return true;
+        else return false;
+    }
 
     public static void main(String [] arg){
         MoreConditionals ce = new MoreConditionals();
         // leap year
-        /*System.out.println("2000 " + ce.isLeapYear(2000));
+        System.out.println("2000 " + ce.isLeapYear(2000));
         System.out.println("2004 " + ce.isLeapYear(2004));
         System.out.println("2003 " + ce.isLeapYear(2003));
         System.out.println("2100 " + ce.isLeapYear(2100));
@@ -49,7 +48,7 @@ public class MoreConditionals {
         System.out.println("1/3/2010 is sooner than 1/2/2010 " + ce.isSooner(1,3,2010, 1,2,2010));
         System.out.println("5/2/2010 is sooner than 1/3/2011 " + ce.isSooner(5,2,2010, 1,3,2011));
         System.out.println("1/2/2010 is sooner than 1/2/2010 " + ce.isSooner(1,2,2010, 1,2,2010));
-        System.out.println("4/8/2010 is sooner than 1/2/2010 " + ce.isSooner(4,8,2010, 1,2,2010));*/
+        System.out.println("4/8/2010 is sooner than 1/2/2010 " + ce.isSooner(4,8,2010, 1,2,2010));
 
         // Best Fit
         System.out.println("Best Fit");
@@ -62,8 +61,17 @@ public class MoreConditionals {
         System.out.println("Find Best fit 4 3 3 is " + ce.findBestFit(4,3,3));
         System.out.println("Find Best fit 4 4 3 is " + ce.findBestFit(4,4,3));
 
-
-
+        // Make Bench
+        System.out.println("Make benches 3 1 8 is " + ce.makeBenches(3,1,8));
+        System.out.println("Make benches 3 1 9 is " + ce.makeBenches(3,1,9));
+        System.out.println("Make benches 11 1 15 is " + ce.makeBenches(11,1,15));
+        System.out.println("Make benches 4 2 15 is " + ce.makeBenches(4,2,15));
+        System.out.println("Make benches 20 0 20 is " + ce.makeBenches(20,0,20));
+        System.out.println("Make benches 3 4 20 is " + ce.makeBenches(3,4,20));
+        System.out.println("Make benches 0 6 30 is " + ce.makeBenches(0,6,30));
+        System.out.println("Make benches 0 5 30 is " + ce.makeBenches(0,5,30));
+        System.out.println("Make benches 2 6 23 is " + ce.makeBenches(2,6,23));
+        System.out.println("Make benches 3 2 23 is " + ce.makeBenches(3,2,23));
 
 }
 }

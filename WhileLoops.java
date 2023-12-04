f84ca93b4ceb7a0a1d8ba7f98863dac19c972897
vaanathisek4r <vaanathisekar@billericak12.com>
2023-12-04 13:59:03 -0500

    finish today

---
+++ WhileLoops.java
@@ -0,0 +1,43 @@
+public class WhileLoops {
+    // PROBLEM 1
+    public int addOdds(int n){
+        int sum = 1;
+        int num = 1;
+    
+        while(num < n){ 
+            num = num + 2;
+            sum = sum + num;
+        }
+        return sum;
+    }
+    // PROBLEM 2 -- UNFINISHED
+    public int sumDigits(int a){
+        int ones = a%
+        while()
+    }
+
+    //PROBLEM 3
+    public int howManyYears(double startpop, double endpop){
+
+
+
+
+
+
+        
+    }
+
+
+
+
+
+
+
+
+    public static void main(String [] args){
+        WhileLoops wl = new WhileLoops();
+        System.out.println(wl.addOdds(5));
+        System.out.println(wl.addOdds(12));
+        System.out.println(wl.addOdds(21));
+    }
+}

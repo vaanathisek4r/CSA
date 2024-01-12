9de27eb596346c6b2d222e0a5a2d1975f9675985
vaanathisek4r <vaanathisekar@billericak12.com>
2024-01-12 13:59:37 -0500

    still working 1/12/24

--- C4 - Iterations (Loops)/StringsExercises.java
+++ C4 - Iterations (Loops)/StringsExercises.java
@@ -13,8 +13,34 @@ public class StringsExercises {
     // PROBLEM 3
     public static String negative(String num) {
         
-        return num.substring(0).replace('0','1').replace('1','0');
+        String ans = num.substring(0).replace('0','2');
+        ans = ans.substring(0).replace('1','0');
+        String ans1 = ans.substring(0).replace('2','1');
+        return ans1;
     }
+
+    // PROBLEM 4
+    public static String dateString(String date) {
+        String month = date.substring(0,2);
+        String day = date.substring(3,5);
+        String year = date.substring(6,10);
+        return day + "-" + month + "-" + year;
+        
+    }
+
+    // PROBLEM 5
+    public static String dateString2(String date) {
+        int i = date.indexOf("/"); // month
+        String month = date.substring(0, i);
+        String moddate = date.substring(i + 1);
+        int j = moddate.indexOf("/"); //day
+        String year = moddate.substring(j + 1);
+        while (i == 1) {
+            month = "0" + month;
+        }
+        String day = "";
+}
+
     public static void main(String[] args) {
         // System.out.println(scroll("Hello World"));
         // System.out.println(scroll("happy"));
@@ -24,8 +50,15 @@ public class StringsExercises {
         // System.out.println(convertName("Biden,Joe"));
         // System.out.println(convertName("the Clown, Bozo"));
 
-        System.out.println(negative("0010111001"));
-        System.out.println(negative("11111111"));
+        // System.out.println(negative("0010111001"));
+        // System.out.println(negative("11111111"));
+
+        //System.out.println("04/20/2014 becomes " + dateString("04/20/2014"));
+
+        System.out.println("04/20/2014 becomes " + dateString2("04/20/2014"));
+        System.out.println("4/20/2014 becomes " + dateString2("4/20/2014"));
+        System.out.println("04/2/2014 becomes " + dateString2("04/2/2014"));
+        System.out.println("4/2/2024 becomes " + dateString2("4/2/2024"));
 
 }
 }

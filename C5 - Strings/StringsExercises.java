3141f82be2977c9d42b1ac67648136828b23ec40
vaanathisek4r <vaanathisekar@billericak12.com>
2024-01-17 13:53:38 -0500

    done 1/17/24

--- C5 - Strings/StringsExercises.java
+++ C5 - Strings/StringsExercises.java
@@ -69,7 +69,7 @@ public class StringsExercises {
     }
     
     public static void main(String[] args) {
-        System.out.println("\scroll");
+        System.out.println("\nscroll");
         System.out.println(scroll("Hello World"));
         System.out.println(scroll("happy"));
         System.out.println(scroll("h"));

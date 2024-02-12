51cb658f0a5421a39040993493821ff4fed36f18
vaanathisek4r <vaanathisekar@billericak12.com>
2024-02-12 13:23:34 -0500

    done 2/12/24

--- C6 - Classes and Arrays/Complex.java
+++ C6 - Classes and Arrays/Complex.java
@@ -25,9 +25,16 @@ public class Complex {
     }
 
     public Complex multiply(Complex other) {
-        return (a * other.a) + (a * other.b) + (b * other.a) + (b * other.b);
+        return new Complex((a * other.a) - (a * other.b), (b * other.a) - (b * other.b));
     }
 
+    public Complex add1(double x) { // Overloaded
+        return new Complex((a + x),(b + x));
+    }
+
+    public Complex multiply1(double x) { // Overloaded
+        return new Complex((a * x), (b * x));
+    }
     public static void main(String[] args) {
         Complex c = new Complex(3,5);
         Complex c2 = new Complex(4,1);
@@ -39,5 +46,9 @@ public class Complex {
         System.out.println(c.toString());
         System.out.println("\nmultiply");
         System.out.println(c.multiply(c2));
+        System.out.println("\nadd1");
+        System.out.println(c.add1(4.0));
+        System.out.println("\nmultiply1");
+        System.out.println(c.multiply1(4.0));
 }
 }

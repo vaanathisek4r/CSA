7c18d027c65fecd3815acd275d81c26ba4437491
vaanathisek4r <vaanathisekar@billericak12.com>
2024-02-12 13:55:19 -0500

    still working 2/12/2024

---
+++ C6 - Classes and Arrays/SoccerTeam.java
@@ -0,0 +1,17 @@
+public class SoccerTeam {
+    private int wins;
+    private int losses;
+    private int ties;
+
+    public void played(SoccerTeam other, int myScore, int otherScore) {
+        if (myScore == otherScore) wins++;
+        else if (win ) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
+
+

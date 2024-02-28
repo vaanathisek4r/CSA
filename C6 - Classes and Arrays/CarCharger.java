c61dfbce5e41f9ef9e5c33c6dfff2539193cded4
vaanathisek4r <vaanathisekar@billericak12.com>
2024-02-28 13:57:10 -0500

    done 2/28

--- C6 - Classes and Arrays/CarCharger.java
+++ C6 - Classes and Arrays/CarCharger.java
@@ -2,7 +2,57 @@ public class CarCharger {
     
     public int getChargingCost(int starthr, int time, int lasthr) {
         int tot_cost = 0;
-        for 
 
+    }
+
+    /** the following rateTable has 24 entries representing the
+     * charging costs for hours 0 through 23.
+     */
+    private int [] rateTable;
+
+    public static void main(String[] args) {
+
+        int [] table = {50,60,160,60,80,100,100,120,150,150,150,
+                200,40,240,220,220,200,200,180,180,140,100,80,60};
+        CarCharger cc = new CarCharger(table);
+        System.out.println("Charging costs");
+        System.out.println("12 1, " + cc.getChargingCost(12,1));
+        System.out.println("0, 2, " +cc.getChargingCost(0, 2));
+        System.out.println("22, 7, " + cc.getChargingCost(22, 7));
+        System.out.println("22, 30, " + cc.getChargingCost(22,30));
+        System.out.println("Best start Time");
+        System.out.println("1 " + cc.getChargeStartTime(1));
+        System.out.println("2 " + cc.getChargeStartTime(2));
+        System.out.println("7 " + cc.getChargeStartTime(7));
+        System.out.println("30 " + cc.getChargeStartTime(30));
+
+
+    }
+    public CarCharger(int[] table){
+        rateTable = table;
+    }
+
+    /** Determines the total cost to charge the car starting at
+     * the beginning of startHour.
+     * @param startHour  the hour when charging begining
+     *                   Precondition: 0 <= startHour <= 23
+     * @param numHours  the number of hours of needed charge.
+     *                  Precondition: chargeTime > 0
+     * @return      the total cost to charge the car.
+     */
+    public int getChargingCost(int startHour, int numHours){
+        
+        return 0;		// replace this
+    }
+
+    /** Determines the start time to charge the car at the
+     * lowest cost for the given charge time.
+     * @param hours  the number of hours needed to charge the car.
+     *               Precondition:  hours > 0
+     * @return an optimal start time with 0 <= returned value <= 23
+     */
+    public int getChargeStartTime(int hours){
+        
+        return -1;   //replace this
     }
 }
--- C6 - Classes and Arrays/GradeAverage.java
+++ C6 - Classes and Arrays/GradeAverage.java
@@ -1,13 +1,3 @@
-/***
-Consider a grading scheme where your final grade is computed more generously
-if your grades show improvement over the term.  Scores show improvement
-if each grade is greater than or equal to the previous grade.
-
-A student has x scores indexed from 0 to x-1.  If the scores have shown improvement
-only those grades with indexes greater than to equal to x/2 are averaged.  If the
-scores have not improved, all of the scores are averaged.
-***/
-
 public class GradeAverage {
 
 	private int [] scores;
@@ -17,42 +7,32 @@ public class GradeAverage {
 		scores = s;
     }
 
-	// returns the mean (average) of the values in the scores array
-	// whose indexes are between first and last (including first and last).  
-	//You may assume that first and last are >= 0 and < scores.length
-
-	private double mean(int first, int last)
-	{
-		// part a
-		return 0.0;    // replace this
-        int avg = 0;
-        for (int i = 0; i < scores.length; i++) {
-            avg += s[i];
+	private double mean(int first, int last) {
+        double sum = 0.0;
+        for (int i = first; i <= last; i++) {
+            sum += scores[i];
         }
-        avg /= scores.length;
+		return sum / (last - first + 1);
 	}
 	
-	// returns true if each successive value in scores is greater than
-	// or equal to the previous value.  Otherwise returns false
-	private boolean showsImprovement()
-	{
-		// part b
-        if (scores <= ){
-
-        }
-		return false;   // replace this
-        
+	private boolean showsImprovement() {
+		boolean improv = true;
+		for (int i = 1; i < scores.length; i++) {
+			if (scores[i] >= scores[i-1]){
+				improv = true;
+			} 
+			else return false;
+		}
+		return improv;
 	}
 	
-	// if the values in the scores array show improvement, returns the
-	// average of the elements in scores with indexes greater than or
-	// equal to scores.length()/2
-	public double finalGrade()
-	{
-		// part c
-        if 
-		return 0.0;   // replace this
+	public double finalGrade() {
+		if (showsImprovement() == true) {
+			return mean(scores.length/2, scores.length-1);
+		}
+		return mean(0, scores.length-1);
 	}
+
 	public static void main(String[] args) {
 		int [] s1 = {50,50,20,80,53};   // not improved, finalGrade is 50.6
 		int [] s2 = {20,50,50,53,80};   // improved, final grade is 61.0
@@ -61,22 +41,22 @@ public class GradeAverage {
 		int [] s5 = {47, 58, 58, 66, 87, 90,90,90};  // improved, final grade is 89.25
 		
 		GradeAverage sr1 = new GradeAverage(s1);
-		//System.out.println(sr1.mean(1,3));
-		//System.out.println(sr1.showsImprovement());
+		System.out.println(sr1.mean(1,3)); // 50.0
+		System.out.println(sr1.showsImprovement()); // false
 		System.out.println(sr1.finalGrade());
 		GradeAverage sr2 = new GradeAverage(s2);
-		System.out.println(sr2.mean(2,4));
-		//System.out.println(sr2.showsImprovement());
+		System.out.println(sr2.mean(2,4)); // 61.0
+		System.out.println(sr2.showsImprovement()); // true
 		System.out.println(sr2.finalGrade());
 		GradeAverage sr3 = new GradeAverage(s3);
-		//System.out.println(sr3.mean(0,2));
-		//System.out.println(sr3.showsImprovement());
+		System.out.println(sr3.mean(0,2)); // 40.0
+		System.out.println(sr3.showsImprovement()); // true
 		System.out.println(sr3.finalGrade());
 		GradeAverage sr4 = new GradeAverage(s4);
-		//System.out.println(sr4.showsImprovement());
+		System.out.println(sr4.showsImprovement()); // false
 		System.out.println(sr4.finalGrade());
 		GradeAverage sr5 = new GradeAverage(s5);
-		//System.out.println(sr5.showsImprovement());
+		System.out.println(sr5.showsImprovement()); // true
 		System.out.println(sr5.finalGrade());		
 	}
 }

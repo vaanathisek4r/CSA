8f2c7f7a8de42297164e6b1eea7f9a51672e5eeb
vaanathisek4r <vaanathisekar@billericak12.com>
2024-03-05 10:18:59 -0500

    working 3/5/24

--- C6 - Classes and Arrays/HarderArraysCSBS.java
+++ C6 - Classes and Arrays/HarderArraysCSBS.java
@@ -1,13 +1,17 @@
 public class HarderArraysCSBS {
     // returns new array a2, with each element value n from a replaced by n consecutive copies of the value n at the same relative location in the array.
     public static String nCopies(int[] a) {
-
+        String new = "";
+        for(int i = 0; i <= a.length + 1; i++) {
+            String += a[i] * i;
+        }
+        
     }
 
 
     public static void main(String[] args) {
         System.out.print("nCopies \n");
-        int[] a = {3,, 5, 0, 2, 2, -7, 0, 4};
+        int[] a = {3, 5, 0, 2, 2, -7, 0, 4};
         int[] a2 = nCopies(a);
         System.out.println(a2);
     }
